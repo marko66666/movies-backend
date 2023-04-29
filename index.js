c429292c05d62a17b7ee823d293dcc5117435989
marko66666 <marko_susa@outlook.com>
2023-04-29 23:06:30 +0200

    Remove cors middleware

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const pool = require("./db");
 require("dotenv").config();
 
-app.use(cors());
 app.use(express.json());
 
 app.use("/auth", require("./routes/jwtAuth"));
